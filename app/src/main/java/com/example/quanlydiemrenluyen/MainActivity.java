fed6dbb10ab1cfa1d27a18780924a48f03e29a07
thanh-sys <phamthanhjob1@gmail.com>
2024-11-11 09:28:48 +0700

    troll

--- app/src/main/java/com/example/quanlydiemrenluyen/MainActivity.java
+++ app/src/main/java/com/example/quanlydiemrenluyen/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     //chautestcommit
+    //cc
+    //okhoi
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
