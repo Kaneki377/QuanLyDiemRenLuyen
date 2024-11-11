e91b002f3db835dc0989df8207ed8faaefced665
thanh-sys <phamthanhjob1@gmail.com>
2024-11-11 09:28:48 +0700

    thanh commit

--- app/src/main/java/com/example/quanlydiemrenluyen/MainActivity.java
+++ app/src/main/java/com/example/quanlydiemrenluyen/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     //chautestcommit
+    //cc
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
